831105cf3dcad322dac71fb7e05424e975d350be
jitendra <jitander51@gmail.com>
2022-11-23 20:09:28 +0530

    Cloudinary for Image

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,9 @@
 const asyncHandler = require('express-async-handler');
 const Product = require("../models/productModel");
 const { fileSizeFormatter } = require("../utils/fileUpload");
+const cloudinary = require('cloudinary').v2;
+
+
 
 const createProduct = asyncHandler(async (req, res) => {
     const { name, sku, category, quantity, price, description } = req.body;
@@ -13,10 +16,19 @@ const createProduct = asyncHandler(async (req, res) => {
 
     // Handle Image upload
     let fileData = {}
-    if(req.file){
+    if (req.file) {
+        // Save image to cloudinary
+        let uploadedFile;
+        try {
+            uploadedFile = await cloudinary.uploader.upload(req.file.path, { folder: "Store Details", resource_type: "image" })
+        } catch (error) {
+            res.status(400);
+            throw new Error("Image could not be uploaded");
+        }
+
         fileData = {
             fileName: req.file.originalname,
-            filePath: req.file.path,
+            filePath: uploadedFile.secure_url,
             fileType: req.file.mimetype,
             fileSize: fileSizeFormatter(req.file.size, 2),
         }
@@ -32,12 +44,12 @@ const createProduct = asyncHandler(async (req, res) => {
         price,
         description,
         image: fileData,
-    }) 
+    })
     res.status(201).json(product);
 
 });
 
- 
+
 
 
 module.exports = {
